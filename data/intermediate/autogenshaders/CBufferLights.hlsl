f622a7556d69fa7afbbfca2a213c946a2a06adb3
Florent Deville <florent.devillechabrol@gmail.com>
2026-02-10 17:19:17 -0800

    Add shader code for directional lights.

--- data/intermediate/autogenshaders/CBufferLights.hlsl
+++ data/intermediate/autogenshaders/CBufferLights.hlsl
@@ -7,18 +7,34 @@
 
 #pragma pack_matrix(row_major)
 
+#define MAX_LIGHT_COUNT 8 
+
+#define DIRECTIONAL_LIGHT 0
+#define POINT_LIGHT 1
+#define SPOT_LIGHT 2
+
 struct Light
 {
 	float3 position;
 	float3 direction;
-	float coneAngle; // only used for spot light
+
+	float3 ambient;
+	float3 diffuse;
+	float3 specular;
+
+	float constantAttenuation;
+	float linearAttenuation;
+	float quadraticAttenuation;
+
+	float cutOff;
+
 	int type; //0 : directional, 1 : point, 2 : spot
 };
 
 cbuffer CBufferLights
 {
 	int numLights;
-	Light lightArray[8];
+	Light lightArray[MAX_LIGHT_COUNT];
 };
 
 #endif // ifndef CBUFFER_LIGHTS_HLSL 
--- source_assets/shaders/base/base.ps.hlsl
+++ source_assets/shaders/base/base.ps.hlsl
@@ -22,22 +22,66 @@ struct VS_Output
 	float3 normal : NORMAL;
 };
 
+float4 CalculateDirectionalLight(Light dirLight, float3 normal, float3 view)
+{
+	//float3 normal = normalize(normal); // Ensure the interpolated normal is a unit vector
+	float3 lightVec = normalize(-dirLight.direction); // Invert the light direction to get L
+
+	// Calculate the dot product and clamp it to a non-negative value using saturate()
+	float diffuseStrength = saturate(dot(normal, lightVec)); //
+	float3 finaDiffuseColor = diffuseStrength * dirLight.diffuse * diffuseColor.xyz;
+	
+	//float3 viewVec = normalize(eyePos - input.worldPos); // Eye position passed from CPU
+	float3 halfVec = normalize(lightVec + view); // Calculate the halfway vector
+
+	// Calculate the dot product of the normal and the halfway vector
+	float specularStrength = pow(saturate(dot(normal, halfVec)), specularPower);
+	float3 finalSpecularColor = specularStrength * dirLight.specular * specularColor.xyz;
+	
+	float3 finalAmbientColor = dirLight.ambient * ambientColor.xyz;
+	//float3 totalLight = finalAmbientColor + finaDiffuseColor + finalSpecularColor;
+	float3 totalLight = finalAmbientColor + finaDiffuseColor;
+
+	return float4(totalLight, 1);
+
+	//float4 finalColor = float4(totalLight, 1.0) * textureColor;
+}
+
 float4 main(VS_Output input) : SV_TARGET
 {
 	float3 normal = normalize(input.normal);
-    //float3 lightDir = normalize(lightArray[0].direction - input.vertex.xyz);
-	float3 lightDir = normalize(lightArray[0].direction);
-    float3 viewDir = normalize(cameraPosition - input.vertex.xyz);
-    float3 reflectionDir = reflect(-lightArray[0].direction, normal);
-	reflectionDir = normalize(reflectionDir);
 	
-    float NdotL = max(0.0f, dot(normal, lightDir));
-    float RdotV = max(0.0f, dot(reflectionDir, viewDir));
+	float4 color = float4(0, 0, 0, 1);
+	
+	if(numLights == 0)
+		color = ambientColor;
+	
+	float3 normalDir = normalize(normal);
+	float3 viewDir = normalize(cameraPosition - input.vertex.xyz);
+	
+	for(int ii = 0; ii < numLights; ++ii)
+	{
+		if(lightArray[ii].type == DIRECTIONAL_LIGHT)
+		{
+			color += CalculateDirectionalLight(lightArray[ii], normalDir, viewDir);
+		}
+		
+		//float3 lightDir = normalize(lightArray[ii].direction - input.vertex.xyz);
+		//float3 lightDir = normalize(lightArray[ii].direction);
+		//float3 viewDir = normalize(cameraPosition - input.vertex.xyz);
+		//float3 reflectionDir = reflect(-lightArray[ii].direction, normal);
+		//reflectionDir = normalize(reflectionDir);
+		
+		//float NdotL = max(0.0f, dot(normal, lightDir));
+		//float RdotV = max(0.0f, dot(reflectionDir, viewDir));
 
-    float4 ambient = ambientColor;
-    float4 diffuse = diffuseColor * NdotL;
-    float4 specular = specularColor * pow(RdotV, specularPower);
+		//float4 ambient = ambientColor;
+		//float4 diffuse = diffuseColor * NdotL;
+		//float4 specular = specularColor * pow(RdotV, specularPower);
 
-    float4 color = ambient + diffuse + specular;
+		//color += ambient + diffuse + specular;
+		//color += diffuse + specular;
+	}
+	
     return diffuseColor * color;
 }

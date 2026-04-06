26ff859ef5bbd34e55dd2c2431c5acf0bdfb5181
Florent Deville <florent.devillechabrol@gmail.com>
2026-04-06 11:12:25 -0700

    [MATERIAL, test_time_animation] Update shader and recompile.

--- source_assets/shaders/test_time_animation/test_time_animation.ps.hlsl
+++ source_assets/shaders/test_time_animation/test_time_animation.ps.hlsl
@@ -22,7 +22,7 @@ float4 main(VS_Output input) : SV_TARGET
 {
 	float3 normal = normalize(input.normal);
 
-	float factor = sin(time * frequency) * amplitude;
+	float factor = (sin(time * frequency) * amplitude) + amplitude;
 	float4 realAmbient = ambient * factor;
 	float4 realDiffuse = diffuse * factor;
 

d7c5d968e8dbeecb72a4105c5f0a378ba7f82a44
Florent Deville <florent.devillechabrol@gmail.com>
2026-04-09 11:26:40 -0700

    [LEVEL, narshe] Improve god rays.

--- source_assets/shaders/god_ray/god_ray.ps.hlsl
+++ source_assets/shaders/god_ray/god_ray.ps.hlsl
@@ -20,21 +20,9 @@ cbuffer PerMaterial
 SamplerState alphaSample;
 Texture2D alpha;
 
-float3 fresnelSchlick(float cosTheta, float3 F0)
-{
-    // F0 is the surface reflection at normal incidence
-    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-// Usage:
-// float3 F = fresnelSchlick(max(dot(normal, view), 0.0), surfaceColor);
-
 float4 main(VS_Output input) : SV_TARGET
 {
 	float3 normal = normalize(input.normal);
-	//float3 view = normalize(cameraPosition - input.worldPosition.xyz); //point to camera 
-
-	//float3 F0 = float3(0.04, 0.04, 0.04);
-	//float3 F = fresnelSchlick(max(dot(normal, view), 0.0), F0);
 
 	float4 diffuseColor = diffuse; //* float4(F, 1);
 	float4 ambientColor = diffuse;
@@ -44,12 +32,15 @@ float4 main(VS_Output input) : SV_TARGET
 	if(numLights != 0)
 	{
 		float3 viewDir = normalize(cameraPosition - input.vertex.xyz);
-		color = CalculateLitColor(ambientColor, diffuseColor, lightArray, input.lightSpacePosition, input.worldPosition, input.normal, viewDir, shadowMap, shadowMapSampler, numLights);
+		color = CalculateLitColor(ambientColor, diffuseColor, lightArray, input.lightSpacePosition, input.worldPosition, normal, viewDir, shadowMap, shadowMapSampler, numLights);
 	}
 
 	float offset = (sin(time * frequency) * amplitude);
 
-	float alphaValue = alpha.Sample(alphaSample, input.uv + (float2)offset).r;
-	color.a = alphaValue;
+	const float PI = 3.14159265;
+
+	float alphaV = sin(input.uv.y * PI);
+	float alphaValue = alpha.Sample(alphaSample, input.uv + float2(offset, 0)).r;
+	color.a = alphaValue * alphaV;
 	return color;
 }
